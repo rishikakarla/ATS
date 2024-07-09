c719615ac86dd8178eeec3646c1c291ed5ade884
Rishi Kakarla <113053189+rishikakarla@users.noreply.github.com>
2024-07-09 08:26:41 +0530

    Update dup.py

--- dup.py
+++ dup.py
@@ -2,14 +2,12 @@ import streamlit as st
 import google.generativeai as genai
 import os
 import PyPDF2 as pdf
-
-import streamlit as st
 from dotenv import load_dotenv
 import json
 
 load_dotenv()  ## load all our environment variables
 
-genai.configure(api_key=os.getenv("AIzaSyAZNiLH-wqNPkl1OtPRkD0BqMPeCAM1gjE"))
+genai.configure(api_key=os.getenv("GOOGLE_API_KEY"))
 
 
 def get_gemini_repsonse(input):
@@ -36,21 +34,17 @@ Hey Act Like a skilled or very experience ATS(Application Tracking System)
 with a deep understanding of tech field,software engineering,data science ,data analyst
 and big data engineer. Your task is to evaluate the resume based on the given job description.
 You must consider the job market is very competitive and you should provide 
-best assistance for improving thr resumes. 
-Profile summary must be based on the resume, not job description. Assign the percentage Matching based 
-on Jd and the missing keywords with high accuracy. 
-Give the existing jobs that matches the profile and can be applied.
-do not create any. Check the skills required and correctly with high accuracy
+best assistance for improving thr resumes. Assign the percentage Matching based 
+on Jd and
+the missing keywords with high accuracy
 resume:{text}
 description:{jd}
-I want the below response in Headings and details format
-{{"Role Match":"%",
+
+I want the below response in paragraph format 
+{{"JD Match":"%",
 "MissingKeywords:[]",
 "Profile Summary":"",
-"Jobs matches with their links":"",
-"recommend courses to learn with links":"",
-"resources for skill improvement (Give certifications)":""
-"possible resume change":""}}
+"recommend courses to learn and resources":""}}
 """
 
 ## streamlit app
@@ -59,17 +53,12 @@ st.text("Improve Your Resume ATS")
 jd = st.text_area("Paste the Job Description")
 uploaded_file = st.file_uploader(
     "Upload Your Resume", type="pdf", help="Please uplaod the pdf"
-import google.generativeai as gen_ai
-
-
-# Load environment variables
-load_dotenv()
-
-# Configure Streamlit page settings
-st.set_page_config(
-    page_title="Chat with Gemini-Pro!",
-    page_icon=":brain:",  # Favicon emoji
-    layout="centered",  # Page layout option
 )
 
 submit = st.button("Submit")
+
+if submit:
+    if uploaded_file is not None:
+        text = input_pdf_text(uploaded_file)
+        response = get_gemini_repsonse(input_prompt)
+        st.subheader(response)
