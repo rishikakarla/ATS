78f4a6af76eca0969fa900bcfc464e6c2752033f
Rishi Kakarla <113053189+rishikakarla@users.noreply.github.com>
2024-08-27 11:42:55 +0530

    Update dup.py

--- dup.py
+++ dup.py
@@ -24,7 +24,7 @@ with a deep understanding of nutrition, physcial excersises, food and humans hea
 You must consider the Healthcare is very competitive and you should provide 
 best assistance for improving the Health Condition.Utilize this information to generate tailored suggestions for BMR of patient and food intake and physical activities. Consider factors such as dietary restrictions, calorie needs, nutritional requirements, and suitable exercise routines based on the given age,height, weight, gender, health condition and allergies and address any specific health concerns the patient may have and profile summary with high accuracy.
 Patient Age:{age}
-patient height:{ln}
+patient height in cm:{cm}
 patient weight:{kg}
 patient Gender:{sex}
 Patient health condition/allergies:{hd}
@@ -42,8 +42,8 @@ I want the below response in paragraph format
 
 st.title("Smart HMA")
 age = st.text_area("Age")
-ln = st.text_area("Height")
-kg = st.text_area("Weight")
+cm = st.text_area("Height in cm")
+kg = st.text_area("Weight in kg")
 sex = st.text_area("Gender")
 hd = st.text_area("Health Conditions/Allergies")
 submit = st.button("Submit")
