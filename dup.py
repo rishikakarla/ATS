9bb3301db0591a7c4ad0d042f8f2483a86e68628
Rishi Kakarla <113053189+rishikakarla@users.noreply.github.com>
2024-08-27 11:24:59 +0530

    Update dup.py

--- dup.py
+++ dup.py
@@ -5,7 +5,7 @@ import PyPDF2 as pdf
 from dotenv import load_dotenv
 import json
 
-load_dotenv()  ## load all our environment variables
+load_dotenv()  
 
 genai.configure(api_key=os.getenv("GOOGLE_API_KEY"))
 
@@ -16,49 +16,24 @@ def get_gemini_repsonse(input):
     return response.text
 
 
-def input_pdf_text(uploaded_file):
-    reader = pdf.PdfReader(uploaded_file)
-    text = ""
-    for page in range(len(reader.pages)):
-        page = reader.pages[page]
-        text += str(page.extract_text())
-    return text
-
-
-# Prompt Template
-
-# I want the response in one single string having the structure
 
 input_prompt = """
-Hey Act Like a skilled or very experience ATS(Application Tracking System)
-with a deep understanding of tech field,software engineering,data science ,data analyst
-and big data engineer. Your task is to evaluate the resume based on the given job description.
-You must consider the job market is very competitive and you should provide 
-best assistance for improving thr resumes. Assign the percentage Matching based 
-on Jd and
-the missing keywords with high accuracy
-resume:{text}
-description:{jd}
+Hey Act Like a skilled or very experience HMA(Health Management Assistence)
+with a deep understanding of nutrition, physcial excersises, food and humans health conditions
+. Your task is to evaluate the Health condition based on the given Patient Profile.
+You must consider the Healthcare is very competitive and you should provide 
+best assistance for improving the Health Condition.Utilize this information to generate tailored suggestions for food intake and physical activities. Consider factors such as dietary restrictions, calorie needs, nutritional requirements, and suitable exercise routines based on the patient's profileand address any specific health concerns the patient may have and profile summary with high accuracy.
+Patient Profile:{pp}
 
 I want the below response in paragraph format 
-{{"JD Match":"%",
-"MissingKeywords:[]",
+{{
+"food intake:[]",
 "Profile Summary":"",
-"recommend courses to learn and resources":""}}
+"recommend physical activities to do":"",
+"calorie needs, nutritional requirements ":"",
+"dietary restrictions":"",}}
 """
 
-## streamlit app
-st.title("Smart ATS")
-st.text("Improve Your Resume ATS")
-jd = st.text_area("Paste the Job Description")
-uploaded_file = st.file_uploader(
-    "Upload Your Resume", type="pdf", help="Please uplaod the pdf"
-)
-
+st.title("Smart HMA")
+jd = st.text_area("Patient Profile")
 submit = st.button("Submit")
-
-if submit:
-    if uploaded_file is not None:
-        text = input_pdf_text(uploaded_file)
-        response = get_gemini_repsonse(input_prompt)
-        st.subheader(response)
